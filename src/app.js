4e71f590203f4e2863c9a39654d718ba3f002080
unknown <singh.avvi98@gmail.com>
2020-12-16 17:24:21 +1100

    C:/Program Files/Git/api/notes GET method operational

--- src/app.js
+++ src/app.js
@@ -1,22 +1,38 @@
 const path = require('path');
-const express = require('express');
 const process = require('process');
+const express = require('express');
+const nanoid = require('nanoid');
+
 
 const app = express();
+
+const notes = require('../db/db.json');
+const publicDirPath = path.join(__dirname, '../public');
 const notesPath = path.join(__dirname, '../public/notes.html');
 const indexPath = path.join(__dirname, '../public/index.html');
 const port = process.env.PORT || 3000
 
-app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(publicDirPath));
+
 
 app.get('/notes', (req, res) => {
     res.sendFile(notesPath);
-})
+});
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
 
 app.get('*', (req, res) => {
     res.sendFile(indexPath);
-})
+});
 
 app.listen(port, () => {
+    let randomID = nanoid.nanoid();
+
     console.log(`server is now listening on port ${port}`);
-})
+    console.log(`random id generation test : ${randomID}`)
+});
