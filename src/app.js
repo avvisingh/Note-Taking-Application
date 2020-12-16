c1292353aa8ee8ccd12b03260292a7604d5e998d
unknown <singh.avvi98@gmail.com>
2020-12-16 15:59:14 +1100

    Basic routing now works and index.html displays when viewing on localhost:3000

--- src/app.js
+++ src/app.js
@@ -1,3 +1,22 @@
+const path = require('path');
 const express = require('express');
+const process = require('process');
 
 const app = express();
+const notesPath = path.join(__dirname, '../public/notes.html');
+const indexPath = path.join(__dirname, '../public/index.html');
+const port = process.env.PORT || 3000
+
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) => {
+    res.sendFile(notesPath);
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(indexPath);
+})
+
+app.listen(port, () => {
+    console.log(`server is now listening on port ${port}`);
+})
